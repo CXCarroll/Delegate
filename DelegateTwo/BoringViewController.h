d3d6edba40a67ad22bc0f2077330b37cc7893660
Cornelius Carroll <CXCarroll@Corneliuss-MacBook-Pro.local>
2013-09-23 11:09:57 -0500

    Initial Commit

---
+++ DelegateTwo/BoringViewController.h
@@ -0,0 +1,13 @@
+//
+//  BoringViewController.h
+//  DelegateTwo
+//
+//  Created by Cornelius Carroll on 9/23/13.
+//  Copyright (c) 2013 Cornelius Carroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoringViewController : UIViewController
+
+@end
---
+++ DelegateTwo/BoringViewController.m
@@ -0,0 +1,38 @@
+//
+//  BoringViewController.m
+//  DelegateTwo
+//
+//  Created by Cornelius Carroll on 9/23/13.
+//  Copyright (c) 2013 Cornelius Carroll. All rights reserved.
+//
+
+#import "BoringViewController.h"
+
+@interface BoringViewController ()
+
+@end
+
+@implementation BoringViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
